ecf60daf7ead7d1f1d3297cabec08e3132eec00b
blacknstones <blacknstones@gmail.com>
2020-11-05 21:51:14 +0100

    Refactored ParcelsPage.

--- src/components/ParcelsPage.jsx
+++ src/components/ParcelsPage.jsx
@@ -3,19 +3,15 @@ import Header from './Header';
 import Parcel from './Parcel';
 
 
-export default function ParcelsPage({information, status}) {
+export default function ParcelsPage({ information }) {
     const Parcels = information.map((item) => {
         return <Parcel key={item.id} data={item} />
     });
-   
-
 
     return (
         <div className="parcels-page">
             <Header />
-                {status === 0 ? <p>Loading...</p> : null}
-                {status === 1 ? <div>{Parcels}</div> : <p>No parcels to show.</p>}
-                {status === 2 ? <p>Sorry we cannot find data</p> : null}
+            <div>{Parcels}</div>
         </div>
     );
 }
