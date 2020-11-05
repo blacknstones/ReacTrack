ab9052656ac40f8122b3fa975131c2cbb88b9cda
blacknstones <blacknstones@gmail.com>
2020-11-05 21:11:56 +0100

    Refactored information flow in pages.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,42 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Other imports
 import './css/style.css';
 import HomePage from './components/HomepPage';
+import ParcelsPage from './components/ParcelsPage';
 
 export default function App() {
 
-  
+  const [status, setStatus] = useState(0);
+    const [information, setInformation] = useState([]);
 
+    const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
+    
+
+    const getData = async () => {
+        try {
+            const response = await fetch(endpoint, { mode: "cors" });
+            const data = await response.json();
+            setInformation(data);
+            setStatus(1);
+
+        } catch (error) {
+            console.log(error);
+            setStatus(2);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/">
-            <HomePage />
+          <Route path="/" exact>
+            <HomePage information={information} status={status}/>
+          </Route>
+          <Route path="/parcels">
+            <ParcelsPage information={information} status={status}/>
           </Route>
         </Switch>
       </div>
--- src/components/HomepPage.jsx
+++ src/components/HomepPage.jsx
@@ -1,50 +1,26 @@
 // React core
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 
 // Components
 import Header from "../components/Header";
-import Parcel from "../components/Parcel";
-
-export default function HomePage() {
-    const [status, setStatus] = useState(0);
-    const [information, setInformation] = useState([]);
-    const [showParcels, setShowParcels] = useState(false);
-
-    const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
-    const Parcels = information.map((item)=> {
-        return <Parcel key={item.id} data={item}/>
-    })
-
-    const getData = async () => {
-        try {
-            const response = await fetch(endpoint, { mode: "cors" });
-            const data = await response.json();
-            setInformation(data);
-            setStatus(1);
-
-        } catch (error) {
-            console.log(error);
-            setStatus(2);
-        }
-    };
-
-    useEffect(() => {
-        getData();
-    }, []);
 
 
-
-
-
+export default function HomePage({information}) {
+    
 
 
     return (
         <div className="home-page">
             <Header />
-            <h1>Home page</h1>
-            <button onClick={() => setShowParcels(!showParcels)}>All Packages</button>
-    <div>{showParcels && Parcels}</div>
-            
+
+            <Link className="button" to="/parcels">
+                All Parcels
+            </Link>
+            <p>Your parcels will be displayed here.</p>
+
+
+
         </div>
     );
 
