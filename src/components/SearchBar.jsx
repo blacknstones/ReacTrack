a2cc08fccbd496dc3fe45f927bdb4354735932db
blacknstones <blacknstones@gmail.com>
2020-11-05 22:13:37 +0100

    Added SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SearchBar() {
+    const [query, setQuery] = useState("");
+    return (
+        <div className="search-bar">
+            <input placeholder="Search with parcel ID" value={query} onChange={(event) => setQuery(event.target.value)} />
+            <Link to={`/results/${query}`}>Search</Link>
+        </div>
+    );
+}
