8a4d7a6739e94b86c237b278dbc1eea2bf1fcefe
blacknstones <blacknstones@gmail.com>
2020-11-07 00:47:02 +0100

    Added not-found-page

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -7,23 +7,16 @@ import ParcelDetail from './ParcelDetail';
 export default function DetailPage({ information }) {
     const { match_id } = useParams();
 
-    {/* The filter function will return an array with one object */}
+    {/* The filter function will return an array with one object */ }
     const matchedItem = information.filter((item) => item.parcel_id.match(match_id))[0];
-
-    if (!matchedItem) {
-        return (
-            <p>Parcel not found!</p>
-        );
-    }
 
     return (
         <div className="detail-page">
             <Header />
             <Link to={"/parcels"}>Go back</Link>
-            <ParcelDetail matchedItem={ matchedItem }/>
-
+            {matchedItem ? <ParcelDetail matchedItem={matchedItem} /> : <p>Parcel not found!</p>}
         </div>
-        
+
     );
 
 }
