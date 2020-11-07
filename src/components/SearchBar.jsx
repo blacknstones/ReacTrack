06bc638649c69dea7f281660b473ec78964e3fb4
blacknstones <blacknstones@gmail.com>
2020-11-07 01:22:21 +0100

    "Updated Search Component"

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function SearchBar() {
     const [query, setQuery] = useState("");
+
+    
+
     return (
         <div className="search-bar">
             <input placeholder="Search with parcel ID" value={query} onChange={(event) => setQuery(event.target.value)} />
